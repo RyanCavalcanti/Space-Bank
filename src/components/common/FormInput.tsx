ce8f00ff6a09f8cb5fac493cea7d4ff60fa64ae3
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-04-09 10:29:06 -0300

    Creating FormInput

---
+++ src/components/common/FormInput.tsx
@@ -0,0 +1,24 @@
+interface FormInputProps {
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
+  type,
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default FormInput;
