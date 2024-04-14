05d0524cb759e9101560a7310298f8c0ba4ae8c0
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-04-13 23:08:01 -0300

    Correcting error of user not being redirected to the dashboard

--- src/pages/Dashboard/AsideDashboard/AsideDashboard.tsx
+++ src/pages/Dashboard/AsideDashboard/AsideDashboard.tsx
@@ -1,17 +1,9 @@
-import { useState, useEffect } from "react";
 import styled from "styled-components";
 import theme from "../../../styles/Theme";
 import Image from "../../../components/common/Image";
 import Title from "../../../components/common/Title";
 import logo from "../../../assets/Icon/Rocket-White.svg";
 import Paragraph from "../../../components/common/Paragraph";
-
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-}
 
 const AsideStyles = styled.aside`
   width: 100%;
@@ -58,34 +50,6 @@ const ContainerHelp = styled.div`
 `;
 
 function AsideDashboard() {
-  const [storedUser, setStoredUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    const fetchStoredUser = async () => {
-      const userString = localStorage.getItem("users");
-      const lastLoginString = localStorage.getItem("lastLogin");
-
-      if (!userString || !lastLoginString) {
-        window.location.replace("/login");
-        return;
-      }
-
-      const users: { [key: string]: User } = JSON.parse(userString);
-      const lastLogin = parseInt(lastLoginString, 10);
-
-      if (Date.now() - lastLogin > 10 * 60 * 1000) {
-        localStorage.removeItem("lastLogin");
-        window.location.replace("/login");
-        return;
-      }
-
-      const user: User = users[lastLogin];
-      console.log("user:", user);
-      setStoredUser(user);
-    };
-
-    fetchStoredUser();
-  }, []);
 
   return (
     <AsideStyles>
@@ -101,11 +65,7 @@ function AsideDashboard() {
           <Title as="h3" color={theme.colors.White}>
             Bem-Vindo!
           </Title>
-          {storedUser !== null && (
-            <Paragraph fontWeight={600} color={theme.colors.White}>
-              {`${storedUser.firstName}`}
-            </Paragraph>
-          )}
+          <Paragraph fontWeight={600} color={theme.colors.White}> Usuário12323 </Paragraph>
         </ContainerUser>
 
         <ContainerBtns>Btns</ContainerBtns>
